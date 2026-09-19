012dc641c61148e90f61986b86994288457fb932
Vikalp Negi <vikalpnegi05@gmail.com>
2026-09-19 14:53:27 +0100

    API key fix and server check

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 from app.routes import router
 
 app = FastAPI(title="Series Rating Tracker API")
